eee5c2a7e9110f7846cf93ee971d142397529265
UtpalB-arch <67811773+UtpalB-arch@users.noreply.github.com>
2021-01-25 06:07:17 +1100

    Update cash_register.js

--- cash_register.js
+++ cash_register.js
@@ -6,11 +6,7 @@ function checkCashRegister(price, cash, cid)
                 let accArray = [];
                 for(let m = 0; m < cid.length; m++){accArray[m]=cid[cid.length - 1 - m];} //console.log(accArray);
 
-                let cashReg = [["ONE HUNDRED", 100.0] ,["TWENTY", 20.0 ],["TEN", 10.0],["FIVE", 5.0], 
-                ["ONE", 1.0],["QUARTER", 0.25],["DIME", 0.1],["NICKEL", 0.05],["PENNY",0.005]]; 
-                cashReg.sort((a,b)=>{return b-a});
-                let outputArray = [];
-                let outputObj = { "status":"", "change": [] };
+                XXXXXXXXXXXXXXXXX PORTION OF CODE DELETED TO PROTECT INTELLECTUAL PROPERTY XXXXXXXXXXXXXXXX
                 
                 for(let i=0; i < cid.length; i++)
                 {cashInBox += cid[i][1];}
@@ -27,38 +23,9 @@ function checkCashRegister(price, cash, cid)
                 let tempcashcount = cashInBox;
                 let allocatedcurr = 0;
 
-                for(let i=0; i < cid.length; i++){
-                    if(changeOwed >= cashReg[i][1]){
-                        let temp1 = 0;
-                        if(changeOwed >= accArray[i][1]){
-                            changeOwed = changeOwed - ((accArray[i][1])/cashReg[i][1])*cashReg[i][1];
-                            changeOwed = Math.round(changeOwed*100)/100;
-                            let tempArray = [];
-                            tempArray.push(accArray[i][0]);
-                            tempArray.push(accArray[i][1]);
-                            //console.log(i,tempArray);
-                            outputArray.push(tempArray);
-                        }
-                        else if(changeOwed < accArray[i][1]){
-                            let tempArray = []; 
-                            let counter = 1;
-                            let tempVar = changeOwed; console.log(tempVar);
-                           do
-                            {tempVar = tempVar - cashReg[i][1]; counter ++;}
-                            while(tempVar/cashReg[i][1] >= 1);                            
-                            console.log(tempVar, cashReg[i][1], counter);
-                            tempArray.push(accArray[i][0]);
-                            tempArray.push(Math.round((changeOwed - tempVar)*100)/100);
-                            changeOwed = tempVar;  
-                            changeOwed = Math.round(changeOwed*100)/100;                      
-                            
-                            //console.log(i,tempArray);
-                            outputArray.push(tempArray);
-                            //changeOwed = 0;
-                        }
-                    }                    
-                } 
-             
+               XXXXXXXXXXXXXXXXXXXXXX PORTION OF CODE DELETED TO PROTECT INTELLECTUAL PROPERTY XXXXXXXXXXXXXXXX
+               
+               
                 if(outputArray.length < 1 || changeOwed > 0){
                     outputObj.status = "INSUFFICIENT_FUNDS";
                     return outputObj;
